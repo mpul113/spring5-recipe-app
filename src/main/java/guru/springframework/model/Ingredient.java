d7667fbead81a773cd78836c7dc080f90ec81f32
Marks ASUS Laptop <pulidomark113@gmail.com>
2022-06-10 16:21:30 -0500

    Feature: Added up to OneToOne UnitOfMeasure

---
+++ src/main/java/guru/springframework/model/Ingredient.java
@@ -0,0 +1,53 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Recipe recipe;
+    private String ingredientDescription;
+    private BigDecimal amount;
+
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getIngredientDescription() {
+        return ingredientDescription;
+    }
+
+    public void setIngredientDescription(String ingredientDescription) {
+        this.ingredientDescription = ingredientDescription;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
